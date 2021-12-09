0edcfb60f81c2384165de1af56be1a3c17000771
Andre Viallon <viallon.andre@gmail.com>
2021-12-09 17:17:21 +0100

    chore: header comp

---
+++ src/app/note/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <mat-toolbar class="flex items-center">
+      <button mat-icon-button (click)="toggleMenu.emit()">
+        <mat-icon>menu</mat-icon>
+      </button>
+      <h1 class="pl-2">You Note</h1>
+    </mat-toolbar>
+  `,
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  @Output() toggleMenu = new EventEmitter();
+}
