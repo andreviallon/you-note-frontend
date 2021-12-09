89ed15c2f94500ebaf46f2d39bd2295896de2016
Andre Viallon <viallon.andre@gmail.com>
2021-12-09 19:05:51 +0100

    chore: side bar

--- src/app/note/sidebar/sidebar.component.ts
+++ src/app/note/sidebar/sidebar.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  template: `<p>it works</p>`,
+  template: `
+    <div class="w-96 h-full p-4 flex flex-col justify-between">
+      <app-notes-list></app-notes-list>
+      <div class="flex justify-between items-center">
+        <span class="text-gray-400">john.doe@gmail.com</span>
+        <button mat-stroked-button color="accent">Logout</button>
+      </div>
+    </div>
+  `,
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class SidebarComponent {}
