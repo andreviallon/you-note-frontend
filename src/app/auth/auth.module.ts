f502f78f6bd2a927ef190406a01f03e7986cf4a1
Andre Viallon <viallon.andre@gmail.com>
2021-12-16 18:22:53 +0100

    chore: register components in auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { NgxsModule } from '@ngxs/store';
 import { UserState } from './state/user.state';
 import { LoginPageComponent } from './connected-components/login-page/login-page.component';
+import { MaterialModule } from '../material/material.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignFormComponent } from './components/sign-form/sign-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const components = [LoginPageComponent, LoginFormComponent, SignFormComponent];
 
 @NgModule({
-  declarations: [
-    LoginPageComponent
+  declarations: [...components],
+  imports: [
+    NgxsModule.forRoot([UserState]),
+    MaterialModule,
+    ReactiveFormsModule,
   ],
-  imports: [NgxsModule.forRoot([UserState])],
-  exports: [],
+  exports: [...components],
 })
 export class AuthModule {}
