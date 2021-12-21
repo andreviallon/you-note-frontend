69b3dc3273add7cfc84456930d141a75e5d0cdcb
Andre Viallon <viallon.andre@gmail.com>
2021-12-21 22:56:47 +0100

    chore: guard notes path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/connected-components/login-page/login-page.component';
+import { AuthGuard } from './auth/auth-guard.service';
 import { NotePageComponent } from './note/connected-components/note-page/note-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  { path: 'notes', component: NotePageComponent },
+  {
+    path: 'notes',
+    component: NotePageComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
